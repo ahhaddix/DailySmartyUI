10034cec9604372bd49524ae704aedfed71465da
ahhaddix <amyhaddix@rocketmail.com>
2022-02-25 19:28:30 -0500

    setup logo component and style

---
+++ src/components/logo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Logo extends Component {
+    render() {
+        return (
+            <div className="logo-main">
+                <img src="/assets/dailysmartyblue.png"/>
+            </div>
+        )
+
+    }
+
+}
+
+export default Logo;
